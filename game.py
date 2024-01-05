ac7a15c400c92d5bd4528fd2a355b657e7c5c39c
Super-Omar <this.omar.abdullah@gmail.com>
2024-01-04 22:11:18 -0800

    remove matplotlib stuff

--- game.py
+++ game.py
@@ -1,8 +1,4 @@
 import random
-import matplotlib
-
-matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -124,45 +120,4 @@ class Game:
             return self.check_win(row, column)
         else:
             raise Exception("Player 1's turn!")
-
-    def draw_board(self):
-        # Create a figure and axis
-        fig, ax = plt.subplots(figsize=(7, 6))
-
-        # Define the colors: empty as white, R as red, and Y as yellow
-        color_dict = {"": "white", "R": "red", "Y": "yellow"}
-
-        # Convert the board to a numerical grid where 0: empty, 1: R, 2: Y for color mapping
-        num_grid = np.zeros((len(self.board), len(self.board[0])), dtype=int)
-        for i, row in enumerate(self.board):
-            for j, cell in enumerate(row):
-                num_grid[i, j] = 0 if cell == "" else (1 if cell == "R" else 2)
-
-        # Create the Connect 4 grid structure
-        ax.matshow(np.ones_like(num_grid), cmap="Blues", vmin=0, vmax=1)
-
-        # Plot the pieces on the board
-        for (i, j), val in np.ndenumerate(num_grid):
-            if val:  # Only draw if the cell is not empty
-                ax.add_patch(
-                    plt.Circle((j, i), 0.45, color=color_dict[self.board[i][j]])
-                )
-
-        # Set the ticks and labels
-        ax.set_xticks(np.arange(len(self.board[0])))
-        ax.set_yticks(np.arange(len(self.board)))
-        ax.set_xticklabels(range(1, len(self.board[0]) + 1))
-        ax.set_yticklabels(range(1, len(self.board) + 1))
-        ax.set_xticks(np.arange(-0.5, len(self.board[0]), 1), minor=True)
-        ax.set_yticks(np.arange(-0.5, len(self.board), 1), minor=True)
-        ax.grid(which="minor", color="black", linestyle="-", linewidth=2)
-
-        # Hide the major tick labels
-        ax.tick_params(which="major", size=0)
-
-        # Set aspect ratio to be equal
-        ax.set_aspect("equal")
-
-        # Show the plot
-        plt.show()
 
